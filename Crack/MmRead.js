63e35dea121a70372aede5af83cdf397e5637cd2
Fantuan-cell <53990888+Fantuan-cell@users.noreply.github.com>
2022-03-09 21:28:56 +0800

    Create MmRead.js

---
+++ Crack/MmRead.js
@@ -0,0 +1,36 @@
+/*
+ *
+ *
+
+脚本功能：咪咪阅读VIP
+网站地址：https://apps.apple.com/cn/app/%E5%92%AA%E5%92%AA%E9%98%85%E8%AF%BB/id1534418454
+脚本作者：Monster
+更新时间：2022-3-9
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+#  开启脚本即是VIP
+^https?:\/\/mmapi02.toolcdn.com\/v1\/my\/info url script-response-body 
+[mitm] 
+
+hostname = mmapi02.toolcdn.com
+
+
+*
+*
+*/
+
+
+
+
+var body = $response.body;
+var hack = /vip_end_date":\d/g;
+body = body.replace(hack,'vip_end_date": 2999');
+$done({
+    body
+});
+
